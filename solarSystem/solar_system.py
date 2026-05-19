9e3cf9b01b1cd7be2d9e5adab4665daa6d92e6f0
bilei <darkdiable@163.com>
2026-05-19 12:56:40 +0800

    fix bug : 目前还是只有太阳，场景中没有其他行星，请修改程序，务必将所有星体都显示出来！

--- solarSystem/solar_system.py
+++ solarSystem/solar_system.py
@@ -2,18 +2,19 @@ from vpython import *
 import math
 
 scene.title = "3D Solar System Simulation"
-scene.width = 1200
-scene.height = 800
+scene.width = 1400
+scene.height = 900
 scene.background = color.black
 scene.autoscale = False
-scene.range = 50
+scene.range = 80
+scene.forward = vector(0, -0.3, -1)
 
 sun_light = local_light(pos=vector(0, 0, 0), color=color.white)
-distant_light(direction=vector(1, 0, 0), color=color.gray(0.3))
+distant_light(direction=vector(1, 0.5, 1), color=color.gray(0.4))
 
 sun = sphere(
     pos=vector(0, 0, 0),
-    radius=4,
+    radius=5,
     color=color.yellow,
     emissive=True,
     shininess=1
@@ -21,9 +22,9 @@ sun = sphere(
 sun_label = label(
     pos=sun.pos,
     text="Sun",
-    yoffset=20,
+    yoffset=30,
     color=color.yellow,
-    height=16,
+    height=18,
     box=False,
     line=False
 )
@@ -31,75 +32,75 @@ sun_label = label(
 planets_data = [
     {
         "name": "Mercury",
-        "radius": 2.5,
-        "orbit_radius": 10,
+        "radius": 3.0,
+        "orbit_radius": 12,
         "orbit_speed": 4.74,
         "rotation_speed": 0.03,
         "color": color.gray(0.7),
-        "texture": textures.rough
+        "start_angle": 0.0
     },
     {
         "name": "Venus",
-        "radius": 3.0,
-        "orbit_radius": 15,
+        "radius": 3.8,
+        "orbit_radius": 18,
         "orbit_speed": 3.50,
         "rotation_speed": 0.005,
         "color": color.orange,
-        "texture": textures.rough
+        "start_angle": 0.8
     },
     {
         "name": "Earth",
-        "radius": 3.2,
-        "orbit_radius": 20,
+        "radius": 4.0,
+        "orbit_radius": 25,
         "orbit_speed": 2.98,
         "rotation_speed": 0.1,
         "color": color.blue,
-        "texture": textures.earth
+        "start_angle": 1.6
     },
     {
         "name": "Mars",
-        "radius": 2.8,
-        "orbit_radius": 25,
+        "radius": 3.4,
+        "orbit_radius": 32,
         "orbit_speed": 2.41,
         "rotation_speed": 0.09,
         "color": color.red,
-        "texture": textures.rough
+        "start_angle": 2.4
     },
     {
         "name": "Jupiter",
-        "radius": 5.0,
-        "orbit_radius": 32,
+        "radius": 6.0,
+        "orbit_radius": 42,
         "orbit_speed": 1.31,
         "rotation_speed": 0.2,
         "color": color.orange,
-        "texture": textures.wood
+        "start_angle": 3.2
     },
     {
         "name": "Saturn",
-        "radius": 4.5,
-        "orbit_radius": 40,
+        "radius": 5.2,
+        "orbit_radius": 52,
         "orbit_speed": 0.97,
         "rotation_speed": 0.18,
         "color": color.yellow.orange(),
-        "texture": textures.rough
+        "start_angle": 4.0
     },
     {
         "name": "Uranus",
-        "radius": 3.8,
-        "orbit_radius": 48,
+        "radius": 4.4,
+        "orbit_radius": 62,
         "orbit_speed": 0.68,
         "rotation_speed": 0.15,
         "color": color.cyan,
-        "texture": textures.rough
+        "start_angle": 4.8
     },
     {
         "name": "Neptune",
-        "radius": 3.6,
-        "orbit_radius": 55,
+        "radius": 4.2,
+        "orbit_radius": 72,
         "orbit_speed": 0.54,
         "rotation_speed": 0.16,
         "color": color.blue,
-        "texture": textures.rough
+        "start_angle": 5.6
     }
 ]
 
@@ -111,25 +112,28 @@ for data in planets_data:
     orbit = ring(
         pos=vector(0, 0, 0),
         radius=data["orbit_radius"],
-        thickness=0.1,
-        color=color.gray(0.4),
+        thickness=0.15,
+        color=color.gray(0.5),
         axis=vector(0, 1, 0)
     )
     orbits.append(orbit)
 
+    start_x = data["orbit_radius"] * math.cos(data["start_angle"])
+    start_z = data["orbit_radius"] * math.sin(data["start_angle"])
+    
     planet = sphere(
-        pos=vector(data["orbit_radius"], 0, 0),
+        pos=vector(start_x, 0, start_z),
         radius=data["radius"],
         color=data["color"],
-        texture=data["texture"],
-        shininess=0.5
+        shininess=0.5,
+        visible=True
     )
     planets.append(planet)
 
     lbl = label(
         pos=planet.pos,
         text=data["name"],
-        yoffset=30,
+        yoffset=35,
         color=color.white,
         height=16,
         box=False,
@@ -139,29 +143,29 @@ for data in planets_data:
 
 saturn_ring = ring(
     pos=planets[5].pos,
-    radius=6.5,
+    radius=7.5,
     thickness=0.3,
     color=color.gray(0.8),
     axis=vector(0.5, 1, 0)
 )
 
 stars = []
-for i in range(200):
+for i in range(300):
     theta = 2 * math.pi * random()
     phi = math.acos(2 * random() - 1)
-    r = 80 + random() * 40
+    r = 100 + random() * 60
     x = r * math.sin(phi) * math.cos(theta)
     y = r * math.sin(phi) * math.sin(theta)
     z = r * math.cos(phi)
     star = sphere(
         pos=vector(x, y, z),
-        radius=0.1,
+        radius=0.15,
         color=color.white,
         emissive=True
     )
     stars.append(star)
 
-angle = [0] * len(planets_data)
+angle = [data["start_angle"] for data in planets_data]
 
 while True:
     rate(60)
